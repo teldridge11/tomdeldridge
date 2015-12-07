651db88013546a6ca60c2ffa4ec05cdab4b4948f
Tom Eldridge <tomeldridge@Toms-MacBook-Air.local>
2015-12-07 17:20:40 -0500

    os

--- tomdeldridge/settings.py
+++ tomdeldridge/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't#5n6--i9*81q6npabad3+@19sio#!$^ltgo=lu^bq+(y*@_pa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.tomdeldridge.com']
 
@@ -103,7 +103,10 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__),'static'),
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
 )
 
 STATIC_ROOT = 'staticfiles'
--- tomdeldridge/wsgi.py
+++ tomdeldridge/wsgi.py
@@ -7,13 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tomdeldridge.settings")
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tomdeldridge.settings")
 
 #application = get_wsgi_application()
