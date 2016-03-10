eb44e5d75ce9e241d4276f8f2982b333113c2bb4
teldridge11 <tom.eldridge@hotmail.com>
2016-03-10 14:58:42 -0500

    prepend www

--- tomdeldridge/settings.py
+++ tomdeldridge/settings.py
@@ -50,6 +50,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
+# Allow URL without www. to redirect to URL with www.
+PREPEND_WWW = True
+
 ROOT_URLCONF = 'tomdeldridge.urls'
 
 TEMPLATES = [
